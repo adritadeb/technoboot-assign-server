10735e8ef4fcadc3eb3902adc493028769597854
adritadeb <adritad2019@gmail.com>
2022-07-19 10:01:08 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-// const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const port = process.env.PORT || 5000;
 const app = express();
@@ -9,19 +9,19 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.btmhcwa.mongodb.net/?retryWrites=true&w=majority`
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.btmhcwa.mongodb.net/?retryWrites=true&w=majority`
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-// async function run(){
-//     try{
-//         await client.connect();
-//         const studentCollection = client.db('stu_info').collection('students');
-//     }
-//     finally{
+async function run(){
+    try{
+        await client.connect();
+        const studentCollection = client.db('stu_info').collection('students');
+    }
+    finally{
 
-//     }
-// }
-// run().catch(console.dir);
+    }
+}
+run().catch(console.dir);
 
 app.get('/',(req,res)=>{
     res.send('Server side is running');
